8ec53d097c269cc1305f1454833c35432ed493bf
Eonjoo-Oh <eonjoo@Ejayui-MacBookAir.local>
2023-06-05 00:37:11 +0900

    event

---
+++ chrome_app/app.js
@@ -0,0 +1,9 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+function onLoginSubmit(event) {
+	event.preventDefault();
+	const username = loginInput.value;
+}
+
+loginForm.addEventListener("submit", onLoginSubmit)
